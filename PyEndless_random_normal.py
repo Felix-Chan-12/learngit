af7d226ef3c025a38b751c0467c61ecfae5a1a4a
Felix_705 <2010024@tongji.edu.cn>
2021-09-29 17:05:09 +0800

    add py file

---
+++ PyEndless_random_normal.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+sampleNo = 1;
+mu = 85;
+sigma = 0.4
+
+for i in range(10):
+	s = np.random.normal(mu, sigma, sampleNo)
+	print(s)
+
+plt.plot(s,'o-')
+plt.show()
